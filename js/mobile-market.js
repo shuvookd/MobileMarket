b52f1fedd9c0b8aeae6b317a15b3bc5b23c4f5dc
shuvokd <shuvokd1210@gmail.com>
2022-03-02 21:49:55 +0600

    Mobile Details

--- js/mobile-market.js
+++ js/mobile-market.js
@@ -43,3 +43,34 @@ const displaySearchResult  = mobile =>{
 const spinnerInDetail = styleD =>{
     document.getElementById('spin2').style.display=styleD;
 };
+// Phone Details 
+const phoneDetails = detailShow =>{
+    const div2 = document.getElementById('search-phone');
+    div2.textContent='';
+    const newDiv2 = document.createElement('div');
+    newDiv2.innerHTML = `
+        <div class="card border-0 p-1 shadow-lg p-3 mb-5 bg-body rounded">
+            <img src="${detailShow.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${detailShow.name}</h5>
+            
+            <p class="card-text fw-bolder">Brand: ${detailShow.brand}</p>
+            <p class="card-text fw-bolder">Release Date: ${detailShow.releaseDate ? detailShow.releaseDate:'Not found Release Date'}</p>
+            <p class="fw-bolder">Main Feature</p>
+            <p class="card-text">Sensors: ${detailShow.mainFeatures.sensors}</p>
+            <p class="card-text">Chipset: ${detailShow.mainFeatures.chipSet}</p>
+            <p class="card-text">Display Size: ${detailShow.mainFeatures.displaySize}</p>
+            <p class="card-text">Storage: ${detailShow.mainFeatures.storage}</p>
+
+            <p class="fw-bolder">Others Feature</p>
+            <p class="card-text">Bluetooth: ${detailShow.others ? detailShow.others.Bluetooth:'Not found bluetooth'}</p>
+            <p class="card-text">GPS: ${detailShow.others ? detailShow.others.GPS:'gps not found'}</p>
+            <p class="card-text">NFC: ${detailShow.others ? detailShow.others.NFC:'nfc not found'}</p>          
+            <p class="card-text">Radio: ${detailShow.others ? detailShow.others.Radio:'radio not found'}</p>
+            <p class="card-text">USB: ${detailShow.others ? detailShow.others.USB:'usb not found'}</p>           
+            <p class="card-text">WLAN: ${detailShow.others ? detailShow.others.WLAN:'wlan not found'}</p>
+            </div>
+        </div>`;
+  div2.appendChild(newDiv2);
+  spinnerInDetail('none')
+}
